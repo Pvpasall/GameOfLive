f3b0e8794b2dd9fe37a4cffc1ed28ffee9be6ef1
Souleymane Sall <papa.sall243@gmail.com>
2023-11-15 21:07:18 +0100

    Petite base de test - Trying to do it with my own kmowledge - HTML/CSS/JS

---
+++ game.js
@@ -0,0 +1,17 @@
+function onClickGetId(id) {
+    console.log(id);
+}
+
+function nombredevoisin(nombre) {
+    if (nombre == 3) {
+        console.log("");
+    }
+}
+
+function addcell(params) {
+    
+}
+
+function killcell(params) {
+    
+}

53c82e4dca0d2ea475b0a07ec5b1df242271f1a1
Souleymane Sall <papa.sall243@gmail.com>
2023-11-18 00:10:31 +0100

    Structure okay !✔. --fix ⛓⛓ resetbutton⛓⛓  --

--- game.js
+++ game.js
@@ -1,38 +1,23 @@
 let cellsOn = [];
 let gameUnlock = false;
-let cellsMatix = new Array();
-let numberOfLine = 5;
-let numberOfColomn = 5;
+let cellsMatrix = new Array();
+let numberOfLine = document.getElementById("numberOfLine").value;
+let numberOfColomn = document.getElementById("numberOfColomn").value;
 
-cellsMatix.length = 5;
+// Au lieu de la repetition sur le html
 
-function addNumOnMatrix() {
-    for (let i = 0; i < cellsMatix.length; i++) {
-        for (let j = 0; j < cellsMatix.length; j++) {
-            if (!cellsMatix[i]) cellsMatix[i] = new Array(); 
-            var case_courante = (i+1) + '-' + (j+1);
-            cellsMatix[i][j] = case_courante;
-            // cellsMatix[i][j] = j;
-            // console.log(cellsMatix[i][j]);
+let myDivTable = document.getElementById("table");
+let myTable = document.createElement("table");
+myTable.setAttribute("id", "myTable")
 
-        }
-    }
-    console.log(cellsMatix);
-}
-
-function showMatrix() {
-    for (let i = 0; i < cellsMatix.length; i++) {
-        for (let j = 0; j < cellsMatix.length; j++) {
-            cellsMatix[i][j] = j;
-            comsole.log(cellsMatix[i][j]);
-        }        
-    }
+function gameStartBtn() {
+    gameUnlock = true;
 }
 
 function gameUnlockBtn() {
     console.log('Game Unlocked');
-    for (let i = 1; i <= numberOfLine; i++) {
-        for (let j = 1; j <= numberOfColomn; j++) {
+    for (let i = 0; i < numberOfLine; i++) {
+        for (let j = 0; j < numberOfColomn; j++) {
             document.getElementById(i+'-'+j).removeAttribute("disabled");
         }
     }
@@ -41,42 +26,108 @@ function gameUnlockBtn() {
 
 function gameLockBtn() {
     console.log('Game locked');
-    for (let i = 1; i <= numberOfLine; i++) {
-        for (let j = 1; j <= numberOfColomn; j++) {
+    for (let i = 0; i < numberOfLine; i++) {
+        for (let j = 0; j < numberOfColomn; j++) {
         document.getElementById(i+'-'+j).setAttribute("disabled", "disabled");
         }
     }
     gameUnlock = false;
 }
 
-function onClickGetId(id) {
-    if (gameUnlock == true) {
-        if (document.getElementById(id).value == 'off') {
-            cellsOn.push(document.getElementById(id).id);
-            document.getElementById(id).value = 'on';
-            console.log(document.getElementById(id).value);
-            console.log(cellsOn);
-        }else{
-            document.getElementById(id).value = 'off';
-            cellsOn.push(document.getElementById(id).id);
-            console.log(document.getElementById(id).value);
-            console.log(cellsOn);
+function gameResetBtn() {
+    document.getElementById("myTable").style.color = 'black';
+}
+
+function generateTableBack(numberOfLine, numberOfColomn) {
+    myTable.setAttribute("border","2px black");
+    myDivTable.appendChild(myTable);
+console.log('numberOfColomn - numberOfLine', numberOfColomn, numberOfLine)
+
+    for (let i = 0; i < numberOfColomn; i++) {
+        let line = document.createElement("tr");
+        for (let j = 0; j < numberOfLine; j++) {
+            if (!cellsMatrix[i]) cellsMatrix[i] = new Array();
+            cellsMatrix[i][j] = document.createElement("td");
+            cellsMatrix[i][j].setAttribute("id",i +'-' +j);
+            cellsMatrix[i][j].setAttribute("onclick",`onClickChangeBg('${i+'-'+j}')`);
+            line.appendChild(cellsMatrix[i][j]);
         }
-    }else{
-        console.log('Start the game');
+        myTable.appendChild(line);
+    }
+    console.log('cellsMatrix', cellsMatrix)
+}
+
+function generateTable() {
+    if (gameUnlock == false)
+        alert('Start the game');
+    else
+        generateTableBack(numberOfLine, numberOfColomn);
+}
+
+// recuperer le nombre de ligne et colonne dans html
+function onChange(id) {
+    if (id == "numberOfLine")
+        numberOfLine = document.getElementById(id).value;
+    else
+        numberOfColomn = document.getElementById(id).value;
+}
+
+function addNumOnMatrix() {
+    for (let i = 0; i < cellsMatrix.length; i++) {
+        for (let j = 0; j < cellsMatrix.length; j++) {
+            if (!cellsMatrix[i]) cellsMatrix[i] = new Array(); 
+            var case_courante = (i+1) + '-' + (j+1);
+            cellsMatrix[i][j] = case_courante;
+            // cellsMatrix[i][j] = j;
+            // console.log(cellsMatrix[i][j]);
+        }
+    }
+    console.log(cellsMatrix);
+}
+
+function showMatrix() {
+    for (let i = 0; i < cellsMatrix.length; i++) {
+        for (let j = 0; j < cellsMatrix.length; j++) {
+            cellsMatrix[i][j] = j;
+            comsole.log(cellsMatrix[i][j]);
+        }        
     }
 }
 
+function onClickChangeBg(id){
+    let cell = document.getElementById(id);
+    let initBgColor = cell.style.backgroundColor;
+    if(cell.style.backgroundColor == 'white'){
+        cell.style.backgroundColor = 'blue';
+        killcell(cellsOn, cell.id);
+        console.log(cellsOn);
+    }else{
+        cell.style.backgroundColor = 'white';
+        cellsOn.push(cell.id);
+        console.log(cellsOn);
+    }
+}
+
+function onClickAddIdOnArray(id) {
+    let cell = document.getElementById(id);
+    if (gameUnlock == true) {
+        cellsOn.push(cell.id);
+        console.log(cellsOn);
+    }else{
+        killcell(cellsOn, cell.id)
+        console.log(cellsOn);
+    }
+}
+
+// ???
 function neighNum(nombre) {
     if (nombre == 3) {
         console.log("");
     }
 }
 
-function addcell(params) {
-    
-}
-
-function killcell(params) {
-    
+function killcell(cellsOn, cell) {
+    let index = cellsOn.indexOf(cell);
+    if (index > -1)
+        cellsOn.splice(index, 1);
 }
